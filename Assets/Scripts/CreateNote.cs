68a47f9edc53c7f8ffb8e1c090d4037028f85cf3
Anna <annashabayev@gmail.com>
2022-03-25 18:57:00 -0400

    moving scripts

--- Assets/Scripts/CreateNote.cs
+++ Assets/Scripts/CreateNote.cs
@@ -17,6 +17,7 @@ public class CreateNote : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1") && fab != null) {
             Rigidbody newBody = Instantiate(fab, new Vector3(0f, 20f, 100f), Quaternion.identity);
+            newBody.transform.parent = gameObject.transform;
             // newBody.AddForce(new Vector3(0f, 1f, -1f) * 100f);
             newBody.velocity = new Vector3(0f, -0.2f, -1f) * 20f;
 
