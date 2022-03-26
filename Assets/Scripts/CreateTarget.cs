e9a3fad7e2f5f91b5619a3955297fe9c3db60398
Alexander Ivkin <a@ivkin.dev>
2022-03-26 11:35:01 -0400

    Play sound on caskets

--- Assets/Scripts/CreateTarget.cs
+++ Assets/Scripts/CreateTarget.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class CreateTarget : MonoBehaviour
 {    
-    public Rigidbody fab;
+    public Rigidbody targetFab;
 
     public void FireTarget(string noteName)
     {
-        Rigidbody newBody = Instantiate(fab, new Vector3(0f, 25f, 120f), Quaternion.identity);
+        Rigidbody newBody = Instantiate(targetFab, new Vector3(0f, 25f, 120f), Quaternion.identity);
         newBody.transform.parent = gameObject.transform;
-        newBody.velocity = new Vector3(0f, -0.25f, -1.2f) * 20f;
-    }
-    
-    public void PlaySound(string noteName) {
+        newBody.velocity = new Vector3(0f, -0.2f, -1f) * 20f;
+
         FindObjectOfType<Audiomanager>().Play("Target_" + noteName);
     }
 }
