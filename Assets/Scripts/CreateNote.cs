f97bbd7119c21f9881a87789b446a21390a830aa
Alexander Ivkin <a@ivkin.dev>
2022-03-26 08:41:17 -0400

    changed logic

--- Assets/Scripts/CreateNote.cs
+++ Assets/Scripts/CreateNote.cs
@@ -2,28 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateNote : MonoBehaviour
-{
-    
+public class CreateTarget : MonoBehaviour
+{    
     public Rigidbody fab;
-    //public GameObject glow;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        /*if (Input.GetButtonDown("Fire1") && fab != null) {
-            Rigidbody newBody = Instantiate(fab, new Vector3(0f, 20f, 100f), Quaternion.identity);
-            newBody.transform.parent = gameObject.transform;
-            // newBody.AddForce(new Vector3(0f, 1f, -1f) * 100f);
-            newBody.velocity = new Vector3(0f, -0.2f, -1f) * 20f;
-
-            //GameObject newGlow = Instantiate(glow, new Vector3(0f, .50f, 100f), Quaternion.identity);
-        }*/
-    }
 
     public void FireTarget()
     {
