c887086febf41675160ac157fbb213ae3e681539
MarshallChadbourne <mchadbourne@berklee.edu>
2022-03-26 09:16:41 -0400

    Added Audio Manager and Sound Class Scripts
    Use these to organize and call audio sources

---
+++ Assets/Audiomanager.cs
@@ -0,0 +1,28 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+ 
+ 
+public class Audiomanager : MonoBehaviour {
+   
+    public Sound[] sounds;
+    
+    //use this for initialization
+    void Awake () {
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume; 
+            s.source.pitch = s.pitch;
+
+        }
+    }
+
+    public void Play (string name)
+    { 
+        Sound s = Array.Find(sounds, sound => sound.name == name); 
+        s.source.Play(); 
+    }
+}
