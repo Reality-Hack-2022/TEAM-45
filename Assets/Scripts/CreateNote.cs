b673ffcff66a6592aad656199c081dddd68ba941
Alexander Ivkin <a@ivkin.dev>
2022-03-25 23:26:37 -0400

    Adjusted the shot

--- Assets/Scripts/CreateNote.cs
+++ Assets/Scripts/CreateNote.cs
@@ -27,7 +27,7 @@ public class CreateNote : MonoBehaviour
 
     public void FireTarget()
     {
-        Rigidbody newBody = Instantiate(fab, new Vector3(0f, 20f, 100f), Quaternion.identity);
+        Rigidbody newBody = Instantiate(fab, new Vector3(0f, 25f, 120f), Quaternion.identity);
         newBody.transform.parent = gameObject.transform;
         newBody.velocity = new Vector3(0f, -0.2f, -1f) * 20f;
     }
--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -33,8 +33,8 @@ public class Shot : MonoBehaviour
 
     private void Enlarge()
     {
-        var scaleTo = scale * 5f;
-        StartCoroutine(ScaleOverSeconds(gameObject, scaleTo, 1.0f));
+        var scaleTo = scale * 15f;
+        StartCoroutine(ScaleOverSeconds(gameObject, scaleTo, 0.5f));
     }
 
     public IEnumerator ScaleOverSeconds(GameObject objectToScale, Vector3 scaleTo, float seconds)
